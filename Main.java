111a58057a72a59862fe846f237a2f0501f65169
DrZaza <zahraanraheel@gmail.com>
2019-10-10 11:24:49 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main
         WA_Strings wa_Strings = new WA_Strings();
         WA_Strings2 wa_Strings2 = new WA_Strings2();
         WA_Strings3 wa_Strings3 = new WA_Strings3();
-        int response = 0;
+        String response = "";
         while(active==true){
             //System.out.println("\u000C\n");
             System.out.println("|=====================================");
@@ -20,24 +20,37 @@ public class Main
             System.out.println("| 2. Worksheet 1");
             System.out.println("| 3. Worksheet 2");
             System.out.println("| 4. Worksheet 3");
+            System.out.println("|Press \"Q\" at any time to Quit");
             System.out.println("|=====================================");
             System.out.println("|Select an Option");
-            response = input.nextInt();
-            if(response == 1){
-                palindrome.Play();
+            
+            response = input.next();
+            switch(response){
+                    case "1":
+                        palindrome.Play();
+                        break;
+                    case "2":
+                        wa_Strings.Play();
+                        break;
+                    case "3":
+                        wa_Strings2.Play();
+                        break;
+                    case "4":
+                        wa_Strings3.Play();
+                        break;
+                    case "q":
+                        System.out.println("Thanks for playing!");
+                        active = false;
+                        break;
+                    case "Q":
+                        System.out.println("Thanks for playing!");
+                        active = false;
+                        break;
+                    default:
+                        System.out.println("That's not a Valid Choice"+
+                        "Try Again!");
             }
-            else if(response == 2){
-                wa_Strings.Play();
-            }
-            else if(response == 3){
-                wa_Strings2.Play();
-            }
-            else if(response == 4){
-                wa_Strings3.Play();
-            }
-            else{
-                System.out.println("That is NOT a Valid Option!");
-            }
+          
         }
     }
 }
