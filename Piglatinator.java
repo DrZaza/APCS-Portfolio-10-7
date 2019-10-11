25a2380b87f80e3e134e3e9b3770ca7ff7a0cad0
DrZaza <zahraanraheel@gmail.com>
2019-10-11 14:04:49 -0700

    Create Piglatinator.java

---
+++ Piglatinator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Piglatinator{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String x = input.nextLine();
+        for(int i = 0; i<x.length();i++){
+            
+        }
+    }
+    public static String translateWord(String word){
+        return "0";
+    }
+}
